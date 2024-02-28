6a54ffe9ead18b4921e7c65145cef6837c03a244
zyansheep <zyansheep@protonmail.com>
2024-02-27 23:03:07 -0500

    some updates

--- cells/nixos/profiles/gaming/common.nix
+++ cells/nixos/profiles/gaming/common.nix
@@ -23,12 +23,4 @@ with lib; {
   programs.gamemode.enable = true;
 
   # nixpkgs.config.allowUnfreePackages = [ "steam" "discord" ];
-
-  # Enable 32 bit libraries for 32-bit games in steam
-  # OpenGL options
-  hardware.opengl = {
-    enable = true;
-    driSupport32Bit = true;
-    extraPackages32 = with pkgs.pkgsi686Linux; [libva];
-  };
 }
