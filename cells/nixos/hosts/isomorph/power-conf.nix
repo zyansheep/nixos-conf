d57bb726814b71e41bb47cb400b0883d0648e5e9
zyansheep <zyansheep@protonmail.com>
2024-04-03 13:59:39 -0400

    move to normal zfs ver

--- cells/nixos/hosts/isomorph/power-conf.nix
+++ cells/nixos/hosts/isomorph/power-conf.nix
@@ -5,10 +5,10 @@
   lib,
   ...
 }: {
-  boot.kernelPackages = pkgs.zfsUnstable.latestCompatibleLinuxPackages;
+  boot.kernelPackages = pkgs.zfs.latestCompatibleLinuxPackages;
 
   # Enable module that exposes battery charge limit (TODO: WHY DOESN'T THIS WORK???)
-  boot.extraModulePackages = with pkgs.zfsUnstable.latestCompatibleLinuxPackages; [framework-laptop-kmod];
+  boot.kernelModules = [ "framework-laptop-kmod" ];
 
   powerManagement = {
     enable = true;
@@ -20,13 +20,14 @@
     "amdgpu.abmlevel=4" # enable adaptive backlight management (Q: does this actually help with power usage?)
   ];
 
-  /*
-     boot.kernelPatches = lib.singleton {
+  
+  /* boot.kernelPatches = lib.singleton {
     name = "enable-cpufreq-stat";
     patch = null;
     extraStructuredConfig = with lib.kernel; {
       CONFIG_CPU_FREQ_STAT = yes;
+      CONFIG_I2C_DESIGNWARE_PLATFORM = yes;
     };
-  };
-  */
+  }; */
+ 
 }
