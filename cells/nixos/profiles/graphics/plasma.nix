2eab47b21cf443247958a80f48d4a81f456a9008
zyansheep <zyansheep@protonmail.com>
2024-07-31 20:18:13 -0400

    revert back to wayland sddm

--- cells/nixos/profiles/graphics/plasma.nix
+++ cells/nixos/profiles/graphics/plasma.nix
@@ -17,11 +17,12 @@ with lib; {
     wayland-utils # for info center
     clinfo # opencl info for info center
   ];
+  services.xserver.enable = true; # can't disable this and enable sddm wayland below as kwallet doesn't unlock
   services.displayManager.sddm = {
     enable = true;
-    wayland = { enable = true; compositor = "kwin"; };
-    theme = "breeze";
-    extraPackages = with pkgs.kdePackages; [ksvg];
+    # wayland = { enable = true; compositor = "kwin"; };
+    # theme = "breeze";
+    # extraPackages = with pkgs.kdePackages; [ksvg];
   };
   services.desktopManager.plasma6.enable = true;
   environment.plasma6.excludePackages = [ pkgs.kdePackages.elisa ];
