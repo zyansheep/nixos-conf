086d042e434e63ab1355afde221654e1f3b30aa4
zyansheep <zyansheep@protonmail.com>
2025-05-26 11:30:40 -0400

    fix cursor icon pack

--- cells/home/profiles/home-manager-base.nix
+++ cells/home/profiles/home-manager-base.nix
@@ -1,12 +1,16 @@
-_: {
-  lib,
-  pkgs,
-  config,
-  ...
-}: {
+_:
+{ lib, pkgs, config, ... }: {
   xdg.enable = true;
 
-  home.file = {
-    ".XCompose" = { source = ./_files/XCompose; };
+  home.file = { ".XCompose" = { source = ./_files/XCompose; }; };
+
+  home.pointerCursor = {
+    name = "Adwaita";
+    package = pkgs.adwaita-icon-theme;
+    size = 24;
+    x11 = {
+      enable = true;
+      defaultCursor = "Adwaita";
+    };
   };
 }
